5a7ba83083e1ff6d422d38fe0e132fd74c9866d7
Anna-Kashyra <anna.kashyra.gmail.com>
2024-10-24 17:26:49 +0300

    Completed homework 4

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -11,23 +11,41 @@ const Pagination: FC<PaginationProps> = ({isFirstPage, isLastPage}) => {
 
     const [query, setQuery] = useSearchParams({page: '1'});
 
+    const getCurrentPage = () => {
+        return Number(query.get('page')) || 1;
+    }
+
     const onClickPrevHandler = () => {
-        const page = query.get('page');
-        if (page && +page > 1) {
-            let currentPage = +page;
-            currentPage--;
-            setQuery({page: currentPage.toString()});
+        const page = getCurrentPage();
+        if (page && page > 1) {
+            const previousPage = page - 1;
+            setQuery({page: previousPage.toString()});
         }
     }
 
+    // const onClickPrevHandler = () => {
+    //     const page = query.get('page');
+    //     if (page && +page > 1) {
+    //         let currentPage = +page;
+    //         currentPage--;
+    //         setQuery({page: currentPage.toString()});
+    //     }
+    // }
+
     const onClickNextHandler = () => {
-        const page = query.get('page');
-        if (page) {
-            let currentPage = +page;
-            currentPage++;
-            setQuery({page: currentPage.toString()});
-        }
+        const page = getCurrentPage();
+        const nextPage = page + 1;
+        setQuery({page: nextPage.toString()});
     }
+
+    // const onClickNextHandler = () => {
+    //     const page = query.get('page');
+    //     if (page) {
+    //         let currentPage = +page;
+    //         currentPage++;
+    //         setQuery({page: currentPage.toString()});
+    //     }
+    // }
 
     return (
         <div className={styles.pagination_container}>
